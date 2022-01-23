163cb02b70663873d49a3db5fb0fd21a77be771b
andrew-wilkes <github@urgentclick.com>
2022-01-23 15:03:02 +0900

    Add code

--- src/Main.gd
+++ src/Main.gd
@@ -1,5 +1,17 @@
 extends Control
 
 func _ready():
-	pass # Replace with function body.
+	add_part_buttons()
 
+func add_part_buttons():
+	for pname in Parts.map.keys():
+		var b = TextureButton.new()
+		b.texture_normal = ResourceLoader.load(Parts.get_part_path(pname))
+		b.connect("pressed", self, "part_pressed", [pname])
+		var c = CenterContainer.new()
+		c.add_child(b)
+		$Top/Parts.add_child(c)
+
+
+func part_pressed(pname):
+	print(pname)
---
+++ src/Parts.gd
@@ -0,0 +1,27 @@
+extends Control
+
+const PART_PATH = "res://assets/parts/small/part-"
+
+var map = {
+	"dc": 3,
+	"ac": 0,
+	"volts": 15,
+	"amps": 1,
+	"r": 12,
+	"c": 2,
+	"pot": 11,
+	"diode": 4,
+	"q": 6,
+	"ecap": 5,
+	"zener": 16,
+	"op": 10,
+	"led": 9,
+	"l": 7,
+	"t1": 13,
+	"t2": 14,
+	"lamp": 8
+}
+
+
+func get_part_path(pname):
+	return PART_PATH + str(map[pname]) + ".png"
