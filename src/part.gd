5aae7c82b24d69e6547f8018d0eff8a9ff67c559
andrew-wilkes <github@urgentclick.com>
2022-01-31 09:22:04 +0900

    Add mirrored ports prop

--- src/part.gd
+++ src/part.gd
@@ -4,12 +4,17 @@ class_name Part
 
 export var flipped = false
 export var is_gnd = false
+export var mirrored_ports = false
 
 const part_vals = [1, 1.2, 1.5, 1.8, 2.2, 2.7, 3.3, 4.7, 6.8, 8.2]
 
 var type = ""
 var data = {} setget set_data, get_data
 var gnd # Reference to ground node
+
+func _ready():
+	pass
+
 
 func set_data(v):
 	data = v
