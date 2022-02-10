27769f8a11a17cb64e2c12bf54422e82b7924d0e
andrew-wilkes <github@urgentclick.com>
2022-02-10 09:34:45 +0000

    Add test code for scope

--- src/Oscilloscope.gd
+++ src/Oscilloscope.gd
@@ -3,7 +3,7 @@ extends Control
 func _ready():
 	yield(get_tree(), "idle_frame")
 	rect_min_size.y = round(0.8 * rect_size.x)
-	get_data_texture()
+	test()
 
 
 func _draw():
@@ -36,22 +36,26 @@ func _draw():
 
 
 # Create an RGBA8 image texture of wave data
-func get_data_texture():
+func test():
 	var rgba = PoolByteArray([])
 	var num_samples = int(rect_size.x)
 	for n in num_samples:
-		var a = 128 + 100 * sin(PI * 4 * n / num_samples)
-		var b = (n % 60) * 250.0 / 60.0
-		var c = 100 if a > 128 else 20
-		var d = n % 50
-		d = n * 3 if a > 128 else -n * 3
+		var a = 128 + 120 * sin(PI * 4 * n / num_samples) # sine wave
+		var b = (n * 4) % 200 + 25 # sawtooth
+		var c = 240 if a > 128 else 20 # square wave
+		var d = (n * 6) % 256
+		if d > 128: d = 256 - d
+		d += 64
 		rgba.append(a)
 		rgba.append(b)
 		rgba.append(c)
 		rgba.append(d)
+	show_traces(rgba)
+
+
+func show_traces(rgba):
 	var img = Image.new()
-	img.create_from_data(num_samples, 1, false, Image.FORMAT_RGBA8, rgba)
+	img.create_from_data(rgba.size() / 4, 1, false, Image.FORMAT_RGBA8, rgba)
 	var texture = ImageTexture.new()
 	texture.create_from_image(img, 0)
 	material.set_shader_param("traces", texture)
-	img.save_png("res://temp.png")
